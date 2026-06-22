3445c000d3b9b1349efc1c57315f548419db1224
Ajim Tamboli <ajim4data@gmail.com>
2026-06-22 22:26:04 +0530

    Added CI/CD to Kubernetes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from DevOps Project 🚀");
+  res.send("Version 2 🚀 Kubernetes Update");
 });
 
 const port = process.env.PORT || 3000;
